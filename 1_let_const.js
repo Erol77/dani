756a8b7908d20ab1b3d50240b33bcf2f707aa25e
Erol <veliev77@mail.ru>
2021-07-01 08:02:12 +0300

    git add .

---
+++ 1_let_const.js
@@ -0,0 +1,32 @@
+/*var num = 42;
+num = 'str';
+console.log(num)*/
+/* let a = 24;
+if (true){
+    let  a= 42;
+    console.log(a,'a');
+}
+console.log(a);
+ */
+//hosting
+
+//const = let
+var a = 24;
+var b = 22;
+var array = [1,2,3,4,5,8];
+//2 стрелочные функции
+function sum(a,b){
+    return a+b;
+}
+function cube(a){
+    return (a*=3 );
+}
+/*const sum1 = (a,b) =>{
+    return a+ b;
+}*/
+//const cube2 = a => a**3 ;//лямбда функции
+/*setTimeout( handler: function(){
+    console.log('After 1 second')
+},timeout:1000)*/
+
+
